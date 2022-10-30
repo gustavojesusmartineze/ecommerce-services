bde179301f19a4189e09938917c78722c5d82ae2
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-30 10:00:53 -0400

    feat(errors): add middlewares for api service

--- api/index.js
+++ api/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const config = require('./../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const errors = require('./../network/errors');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler
+} = require('./../network/error.handler');
 
 const app = express();
 
@@ -14,7 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 
-app.use(errors);
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(config.api.port, () => {
   console.log('API listening on port ', config.api.port);
